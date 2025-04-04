4c31c30d834718c52b2abfc97a7ded43b5a5a381
BarhayoB <barhayob@gmail.com>
2025-04-04 16:02:07 +0500

    bir

---
+++ bir.js
@@ -0,0 +1,20 @@
+let kattaRasm = document.querySelector('.katta-rasm');
+let rasm1 = document.querySelector('.rasm1');
+let rasm2 = document.querySelector('.rasm2');
+let rasm3 = document.querySelector('.rasm3');
+let rasm4 = document.querySelector('.rasm4');
+
+rasm1.addEventListener('click', function () {
+    kattaRasm.src = rasm1.src;
+});
+rasm2.addEventListener('click', function () {
+    kattaRasm.src = rasm2.src;
+});
+rasm3.addEventListener('click', function () {
+    kattaRasm.src = rasm3.src;
+});
+rasm4.addEventListener('click', function () {
+    kattaRasm.src = rasm4.src;
+});
+
+
